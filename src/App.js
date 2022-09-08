2f3553bf0d09c80cc64c4f8de72e7f43213a83a7
chetankochiyaniya <60025285+chetankochiyaniya@users.noreply.github.com>
2022-09-08 16:46:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/homeland' element={<Home />} />
         <Route path='/property/:id' element={<PropertyDetails />} />
       </Routes>
       <Footer />
